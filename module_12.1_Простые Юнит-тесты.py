4376d38631aa5dc949c5d9546b7c31c5323ca214
Vadim55-omsk <fvg-income@yandex.ru>
2024-10-19 18:22:57 +0300

    Add files via upload

---
+++ module_12.1_Простые Юнит-тесты.py
@@ -0,0 +1,33 @@
+import unittest
+
+from runner import Runner
+
+
+class RunnerTest(unittest.TestCase):
+
+    def test_walk(self):
+        runner = Runner("Test Walker")
+        for _ in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+
+    def test_run(self):
+        runner = Runner("Test Runner")
+        for _ in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge(self):
+        runner1 = Runner("Runner")
+        runner2 = Runner("Walker")
+
+        for _ in range(10):
+            runner1.run()
+            runner2.walk()
+
+        self.assertNotEqual(runner1.distance, runner2.distance)
+
+
+# Запуск тестов
+if __name__ == "__main__":
+    unittest.main()
